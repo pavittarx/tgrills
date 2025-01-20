ff03f228529127fcfa1a50196f7e39d9c00be1d1
pavittarx <29155477+pavittarx@users.noreply.github.com>
2025-01-20 17:16:31 +0000

    feat: add scrollability to the top bar

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import { getCatalogue } from "./_services";
 
 import { useState, useEffect } from "react";
 import classNames from "classnames";
-import { MotionConfig } from "motion/react";
+import { motion, MotionConfig } from "motion/react";
 import { MdClose } from "react-icons/md";
 
 type Category = {
   id: string;
   name: string;
-}
+};
 
 export default function Home() {
   const products = useProducts((s) => s.products);
@@ -28,7 +28,7 @@ export default function Home() {
 
   useEffect(() => {
     (async () => {
-      const {products, categories, banners} = await getCatalogue();
+      const { products, categories, banners } = await getCatalogue();
       setProducts(products);
       setCategories(categories.map((cat: Category) => cat.name));
       setBanners(banners);
@@ -36,59 +36,78 @@ export default function Home() {
   }, []);
 
   return (
-      <main className="h-full">
-        <section className="border border-gray-100 py-2 mb-1 flex">
-          {categories.map((category) => {
-            const isActive = filters.includes(category);
+    <main className="h-full">
+      <section className="mx-2 py-2 mb-1 flex overflow-x-scroll no-scrollbar">
+        {categories.map((category) => {
+          const isActive = filters.includes(category);
 
-            return <button
-              key={category}
-              className={classNames(
-                "px-3 py-1 mx-1 text-xs rounded-full hover:bg-gray-300",
-                "border",
-                "flex items-center gap-1",
-                isActive && "bg-gray-100"
-              )}
-              onClick={() => {
-                if (!isActive) {
-                  const _filters = [...filters, category];
-                  setFilters(_filters);
-                } else {
-                  setFilters(filters.filter((f) => f !== category));
-                }
-              }}
-            >
-              {category}
-              {isActive && <div className="m-[0.5] p-0.5 border rounded-lg border-zinc-300"> <MdClose /> </div>}
-            </button>
-          })}
-        </section>
+          return (
+            <motion.div key={category}>
+              <motion.button
+                animate={true}
+                transition={{ duration: 1 }}
+                className={classNames(
+                  "px-2 py-1 mx-1 h-8 text-xs rounded-full select-none",
+                  "min-w-14 text-center hover:bg-gray-50",
+                  "whitespace-nowrap",
+                  "border block",
+                  "flex items-center gap-1 flex-1",
+                  isActive && "bg-gray-100"
+                )}
+                onClick={() => {
+                  if (!isActive) {
+                    const _filters = [...filters, category];
+                    setFilters(_filters);
+                  } else {
+                    setFilters(filters.filter((f) => f !== category));
+                  }
+                }}
+              >
+                <span className="w-full">{category}</span>
+                {isActive && (
+                  <div className="m-[0.5] p-0.5 border rounded-lg border-zinc-300">
+                    {" "}
+                    <MdClose />{" "}
+                  </div>
+                )}
+              </motion.button>{" "}
+            </motion.div>
+          );
+        })}
+      </section>
 
-        <section className="m-1 h-[200px] w-full">
-          <Carousel slides={bannersTop} options={{ loop: true }} />
-        </section>
+      <section className="m-1 h-[200px] w-full">
+        <Carousel slides={bannersTop} options={{ loop: true }} />
+      </section>
 
-        <div className="grid grid-cols-1 gap-2 mb-4">
-          <MotionConfig
-            transition={{
-              duration: 5,
-              ease: "easeInOut",
-              type: "spring",
-              stiffness: 100,
-            }}
-          >
-            {products
-              .filter((product) =>
-                filters.length > 0
-                  ? product.categories.some((cat) => filters.includes(cat))
-                  : true
-              )
-              .map((product) => (
-                <Product key={product.name} {...product} />
-              ))}
-          </MotionConfig>
-        </div>
-        
-      </main>
+      <div className="grid grid-cols-1 gap-2 mb-4">
+        <MotionConfig
+          transition={{
+            duration: 5,
+            ease: "easeInOut",
+            type: "spring",
+            stiffness: 100,
+          }}
+        >
+          {products
+            .filter((product) => {
+              console.log(product.categories);
+
+              if (filters.length > 0) {
+                const res = filters.every((cat) =>
+                  product.categories.includes(cat)
+                );
+                console.log(res);
+
+                return res;
+              }
+              return true;
+            })
+            .map((product) => (
+              <Product key={product.name} {...product} />
+            ))}
+        </MotionConfig>
+      </div>
+    </main>
   );
 }
