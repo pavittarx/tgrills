f64e625c7b652e8f6765bccc226542a5acc3d318
pavittarx <29155477+pavittarx@users.noreply.github.com>
2025-01-20 10:44:02 +0000

    fix: rework, cart & store integration

---
+++ app/_shared/Quantity.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import { MdOutlineAdd, MdOutlineRemove, MdOutlineDelete } from "react-icons/md";
+
+type QuantityProps = {
+  quantity: number;
+  onAdd: () => void;
+  onRemove: () => void;
+  onDelete: () => void;
+};
+
+type ButtonProps = {
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+const Button: FC<ButtonProps> = ({ onClick, children }) => {
+  return (
+    <button
+      className="h-[30px] w-[30px] bg-yellow-50 text-yellow-900 shadow shadow-yellow-100 rounded"
+      onClick={e => {
+        e.stopPropagation();
+        onClick();}}
+    >
+      {children}
+    </button>
+  );
+};
+
+export const Quantity: FC<QuantityProps> = ({
+  quantity,
+  onAdd,
+  onRemove,
+  onDelete,
+}) => {
+  return (
+    <div className="text-base flex gap-1 items-center">
+      <Button onClick={quantity > 1 ? onRemove : onDelete}>
+        {quantity > 1 && <MdOutlineRemove className="inline-block" />}
+        {quantity == 1 && <MdOutlineDelete className="inline-block" />}
+      </Button>
+      <span className="bg-white p-1 text-xs text-center w-[22px]">
+        {quantity}
+      </span>
+      <Button onClick={onAdd}>
+        <MdOutlineAdd className="inline-block" />
+      </Button>
+    </div>
+  );
+};
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,35 +1,56 @@
 "use client";
 
-import { useCart } from "../_store"
+import { useCart, useProducts } from "../_store";
+import { Quantity } from "../_shared/Quantity";
 
-export default function Page(){
-    const cart = useCart(s => s.cart);
-    console.log(cart);
+export default function Page() {
+  const cart = useCart((s) => s.cart);
+  const products = useProducts(s => s.products);
+  const handleAdd  = useCart((s) => s.addQuantity);
+  const handleRemove = useCart((s) => s.removeQuantity);
+  const handleDelete = useCart((s) => s.removeFromCart);
 
-    const header = {
-        name: 'Name',
-        quantity: 'Quantity',
-        price: 'Price',
-        total: 'Total',
-    }
+  if(!products.length){
+    return <>No Products</>;
+  }
 
-    return <main className="mx-2 my-4">
-        <h1 className="mx-2 pl-4 pb-1 font-semibold border-b grid-cols-4">Cart</h1>
+  const _cart = cart.map((item) => {
+    const product = products.find(p => p.id === item.id)!;
+    return {
+      ...item,
+      ...product
+    };
+  });
 
-        <section className="py-2">
-            {
-                ...cart.map((product) => {
-                    return <div 
-                                key={product.name} 
-                                className="py-1 text-xs pl-4 grid grid-rows-1 grid-cols-[200px_50px_50px_50px_50px] gap-1">
-                                    <div className="col-span-1">{product.name}</div>
-                                    <span className="inline-block col-span-1 place-self-end">{product.quantity}</span>
-                                    <div className="col-span-1 place-self-end">{product.price}</div>
-                                    <div className="col-span-1 place-self-end">{product.quantity * parseFloat(product.price)}</div>
-                                    <div className="col-span-1 place-self-end"> D </div>
-                            </div>
-                })
-            }
-        </section>
+  return (
+    <main className="mx-2 my-4">
+      <h1 className="mx-2 pl-4 pb-1 font-semibold border-b grid-cols-4">
+        Cart
+      </h1>
+
+      <section className="py-2">
+        {_cart.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="w-full py-1 text-xs pl-6 grid grid-rows-1 grid-cols-[200px_80px_20px] gap-1 items-center justify-between"
+            >
+              <div className="col-span-1">{item.name}</div>
+              <span className="inline-block col-span-1">
+                <Quantity
+                  quantity={item.quantity}
+                  onAdd={() => {handleAdd(item.id)}}
+                  onRemove={() => handleRemove(item.id)}
+                  onDelete={() => handleDelete(item.id)}
+                /> 
+              </span>
+              <div className="col-span-1 self-place-end text-right">
+                {item.quantity * parseFloat(item.price)}
+              </div>
+            </div>
+          );
+        })}
+      </section>
     </main>
+  );
 }
