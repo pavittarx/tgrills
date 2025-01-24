bd558298f2112e704ffd3d83e28780571936fa12
pavittarx <pavittarx>
2025-01-24 11:30:49 +0000

    feat: configure supabase

---
+++ app/_sdk/supabase.ts
@@ -0,0 +1,4 @@
+import {createClient} from "@supabase/supabase-js";
+import { SUP_CLIENT_KEY, SUP_PROJECT_URL } from "../config";
+
+export const sup = createClient(SUP_PROJECT_URL!, SUP_CLIENT_KEY!);
---
+++ app/config.ts
@@ -0,0 +1,6 @@
+const secrets = process.env;
+
+export const SHEET_ID = secrets.SHEET_ID;
+export const GOOGLE_AUTH_CREDS = secrets.G_CREDS;
+export const SUP_PROJECT_URL =  secrets.NEXT_PUBLIC_SUP_PROJECT_URL;
+export const SUP_CLIENT_KEY = secrets.NEXT_PUBLIC_SUP_CLIENT_KEY;
