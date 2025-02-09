d9b2fd5124324fd9192b9bd5b94795fa6d6cdb51
pavittarx <29155477+pavittarx@users.noreply.github.com>
2025-02-09 12:22:21 +0000

    fix: buttons on cart page

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -26,6 +26,7 @@ const buttonVariants = cva(
         out: "bg-amber-100 text-amber-800 shadow-sm hover:bg-amber-200",
         delivered: "bg-green-100 text-green-800 shadow-sm hover:bg-green-200",
         cancelled: "bg-red-100 text-red-800 shadow-sm hover:bg-red-200",
+        cart: "bg-yellow-300 text-yellow-900 shadow-sm hover:bg-yellow-400 hover:shadow-md transition-all duration-150 ease-in-out",
       },
       size: {
         default: "h-9 px-4 py-2",
