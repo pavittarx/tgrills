c13d0ac14255b50946c79ca64cb0e60fbd067ba4
pavittarx <29155477+pavittarx@users.noreply.github.com>
2025-02-22 02:02:51 +0000

    fix: source banners from supabase

--- app/_shared/Carousel.tsx
+++ app/_shared/Carousel.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { EmblaOptionsType } from "embla-carousel";
 import useEmblaCarousel from "embla-carousel-react";
+import { getPublicImageUrl } from "@/_sdk/supabase";
+import Image from "next/image";
 
 type PropType = {
   slides: string[];
@@ -12,30 +14,29 @@ export const Carousel: React.FC<PropType> = (props) => {
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
   const [focus, setFocus] = useState<boolean>(false);
 
-
   useEffect(() => {
-    if(!emblaApi) return;
+    if (!emblaApi) return;
 
     const _scrollId = setInterval(() => {
-      if(!focus){
+      if (!focus) {
         emblaApi?.scrollNext();
       }
     }, 5000);
 
     return () => {
       clearInterval(_scrollId);
-    }
+    };
   }, [emblaApi]);
 
   useEffect(() => {
     emblaApi?.on("pointerDown", () => {
       setFocus(true);
-    })
+    });
 
     emblaApi?.on("pointerDown", () => {
       setFocus(false);
     });
-  }, [])
+  }, []);
 
   return (
     <section className="embla">
@@ -43,8 +44,13 @@ export const Carousel: React.FC<PropType> = (props) => {
         <div className="embla__container">
           {slides.map((slide, index) => (
             <div className="embla__slide" key={index}>
-              <div className="embla__slide__number">
-                <img src={slide} height={"100%"} width={"100%"} alt="Banner" />
+              <div className="embla__slide__number object-cover">
+                <Image
+                  src={getPublicImageUrl(slide)}
+                  height={150}
+                  width={500}
+                  alt="Banner"
+                />
               </div>
             </div>
           ))}
